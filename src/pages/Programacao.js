1b85538584aee4c786071b768b4dcbd5d57c91cc
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2023-06-21 20:13:43 -0300

    cardsProgramacaoStyles

--- src/pages/Programacao.js
+++ src/pages/Programacao.js
@@ -1,6 +1,7 @@
 import './Programacao.css'
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import {SiUndertale} from 'react-icons/si';
 
 function Programacao() {
 
@@ -10,6 +11,7 @@ function Programacao() {
 
 
     useEffect(() => {
+     
         axios.get('http://localhost:4000/cards')
             .then((response) => {
                 console.log(response.data)
@@ -54,7 +56,7 @@ function Programacao() {
                                    
 
                                    
-                                    <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+                                    <a class="btn btn-primary mb-3 mr-1" href="#carouselExampleIndicators1" role="button" data-slide="prev">
                                         <i class="fa fa-arrow-left"></i>
                                     </a>
                                     <a class="btn btn-primary mb-3 " href="#carouselExampleIndicators2" role="button" data-slide="next">
@@ -62,6 +64,9 @@ function Programacao() {
                                     </a>
                                 </div>
                                 <div class="col-12">
+
+                                        
+
                                     <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carousel">
 
                                         <div class="carousel-inner">
@@ -82,12 +87,16 @@ function Programacao() {
 
                                                     
                                                                     <div  class="card">
+                                                                        <div className='container-img'>
+                                                                           
+                                                                       
                                                                         <img className="rounded-circle image-style" alt="imagem timee" src={filteredtime.imagem} height={'250px'} width={'300px'} />
+                                                                        </div>
                                                                         <div class="card-body">
                                                                             <h4 class="card-title">{filteredtime.nome}</h4>
-                                                                            <p class="card-text">{filteredtime.cargo}</p>
-
-                                                                        </div>
+                                                                            <p class="card-text"><span className='span-programacao'><SiUndertale className="icon-time"></SiUndertale> Cargo:</span> {filteredtime.cargo}</p>
+                                                                    </div>
+                                                                        
                                                                     </div>
                                                                 </div>
                                                                    ))}
